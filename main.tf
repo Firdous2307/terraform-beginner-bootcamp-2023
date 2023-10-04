051fcf28dab246665fd65ba81daf2961351029fa
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-10-04 18:47:26 +0000

    #37 Assets Upload and For Each

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ terraform {
   #  }
   #}
 }
+
 module "terrahouse_aws" {
 source = "./modules/terrahouse_aws"
 user_uuid = var.user_uuid
@@ -21,4 +22,5 @@ bucket_name = var.bucket_name
 index_html_filepath = var.index_html_filepath
 error_html_filepath = var.error_html_filepath
 content_version = var.content_version
+assets_path = var.assets_path
 }  
