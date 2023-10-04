260f6564b38beaff7b4a5d077deed7517c0bfb4b
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-10-04 03:51:41 +0100

    #25 Refactor the S3 bucket configuration into the newly created terrahouse_aws module (#26)

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+# [AWS S3 Bucket Resource Docs]
+# Available at:(https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket)
+resource "aws_s3_bucket" "website_bucket" {
+# [S3 Bucket Naming Rules]
+# Available at:(https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console)
+bucket = var.bucket_name
+tags = {
+    UserUuid = var.user_uuid
+  }
+}  
