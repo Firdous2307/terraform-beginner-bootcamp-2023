1b76ff9ca543b1e5da5502e1ab14ed1ad0db4d8a
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-10-04 02:39:47 +0100

    #23 Refactored the terraform configuration for  S3 bucket (#24)

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-# [Random String Resource Docs]
-# Available at:(https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string)
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length   = 32
-  special  = false
-}
-
 # [AWS S3 Bucket Resource Docs]
 # Available at:(https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket)
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "website_bucket" {
 # [S3 Bucket Naming Rules]
 # Available at:(https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console)
-bucket = random_string.bucket_name.result
+bucket = var.bucket_name
 tags = {
     UserUuid = var.user_uuid
   }
