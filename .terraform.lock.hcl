17ee87906920ae6343d45e7f3fbe8c3f7e377a48
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-10-10 02:38:02 +0100

    #51 working on my multi terrahomes (#52)

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,7 +5,7 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:GQnzn2/4caEuJoPp9BkvntM6bJg6hjYbS8R2EHUxnlM=",
+    "h1:I8HaRAydN8EjmA8JhQOfxSCDE+AawQI1bWhPkXNkd1U=",
   ]
 }
 
--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ terraform {
   #    name = "terra-house-01"
   #  }
   #}
-  #cloud {
-  #  organization = "mohfird23"
-  #  workspaces {
-  #    name = "terra-house-01"
-  #  }
-  #}
+  cloud {
+    organization = "mohfird23"
+    workspaces {
+      name = "terra-house-01"
+    }
+  }
 }
 provider "terratowns" {
   endpoint = var.terratowns_endpoint
@@ -27,23 +27,126 @@ provider "terratowns" {
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_anime_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.anime.public_path
+  content_version = var.anime.content_version
 }
 
 
-resource "terratowns_home" "home" {
-  name = "Let's talk about Naruto!!"
+resource "terratowns_home" "home_anime" {
+  name = "Let's talk about Anime!!"
   description = <<DESCRIPTION
-Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+Anime is Japanese animation encompassing hand-drawn and computer-generated styles.
+In Japan, it refers to all animated works, while outside Japan, it specifically denotes Japanese-produced animation.
+Elements of anime can also appear in video games and animations worldwide.
+DESCRIPTION
+  domain_name = module.home_anime_hosting.domain_name
+  town = "missingo"
+  content_version = var.anime.content_version
+}
+
+
+module "home_r-b_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.r-b.public_path
+  content_version = var.r-b.content_version
+}
+
+resource "terratowns_home" "home_r-b" {
+  name = "Welcome to my Music Realm"
+  description = <<DESCRIPTION
+Let's talk about my favorite genre which is Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
 It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. 
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
-  town = "missingo"
-  content_version = 1
+  domain_name = module.home_r-b_hosting.domain_name
+  town = "melomaniac-mansion"
+  content_version = var.r-b.content_version
 }
+
+
+module "home_naija-jollof-rice_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.naija-jollof-rice.public_path
+  content_version = var.naija-jollof-rice.content_version
+}
+
+resource "terratowns_home" "home_naija-jollof-rice" {
+  name = "Welcome to my Music Realm"
+  description = <<DESCRIPTION
+Let's talk about my favorite genre which is Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. 
+DESCRIPTION
+  domain_name = module.home_naija-jollof-rice_hosting.domain_name
+  town = "cooker-cove"
+  content_version = var.naija-jollof-rice.content_version
+}
+
+
+
+module "home_christopher-nolan_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.christopher-nolan.public_path
+  content_version = var.christopher-nolan.content_version
+}
+
+resource "terratowns_home" "home_christopher-nolan" {
+  name = "Welcome to my Music Realm"
+  description = <<DESCRIPTION
+Let's talk about my favorite genre which is Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. 
+DESCRIPTION
+  domain_name = module.home_christopher-nolan.domain_name
+  town = "video-valley"
+  content_version = var.christopher-nolan.content_version
+}
+
+
+
+module "home_lagos-life_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.lagos-life.public_path
+  content_version = var.lagos-life.content_version
+}
+
+resource "terratowns_home" "home_lagos-life" {
+  name = "Welcome to my Music Realm"
+  description = <<DESCRIPTION
+Let's talk about my favorite genre which is Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. 
+DESCRIPTION
+  domain_name = module.home_lagos-life.domain_name
+  town = "the-nomad-pad"
+  content_version = var.lagos-life.content_version
+}
+
+
+
+
+module "home_fifa-23_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.fifa-23.public_path
+  content_version = var.fifa-23.content_version
+}
+
+resource "terratowns_home" "home_fifa-23" {
+  name = "Welcome to my Music Realm"
+  description = <<DESCRIPTION
+Let's talk about my favorite genre which is Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. 
+DESCRIPTION
+  domain_name = module.home_fifa-23.domain_name
+  town = "gamers-grotto"
+  content_version = var.fifa-23.content_version
+}
+
+
+
+
+
